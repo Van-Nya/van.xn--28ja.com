7d2f666429ed085d5275ab363de06f46a16ba6a2
Van-Nya <admin@ruavan.moe>
2021-04-01 17:10:26 +0800

    Site updated at 2021-04-01 17:10:26

---
+++ js/404.js
@@ -0,0 +1,22 @@
+/*
+ * Van_Nya's 404 jumping script
+ */
+
+const HOME = new URL(window.location.href).origin
+const TIMEOUT = 1000, DELAY = 5;
+document.getElementById('page-site-info').innerHTML +=
+'<div id="site-subtitle">' +
+    '<span id="timeout">' + String(DELAY) + '</span>' + '秒后自动跳转回主页' +
+'</div>';
+
+(function () {
+    let _delay = DELAY;
+    setInterval(function () {
+        if (_delay === 0) {
+            window.location.href = String(HOME);
+        } else {
+            _delay--;
+            document.getElementById('timeout').innerHTML = String(_delay);
+        }
+    }, TIMEOUT)
+})()
