60be73b64ab65344dadf52c07084b1ec894fc66a
Van-Nya <admin@ruavan.moe>
2021-04-12 21:33:56 +0800

    Site updated at 2021-04-12 21:33:56

--- js/404.js
+++ js/404.js
@@ -5,9 +5,7 @@
 const HOME = new URL(window.location.href).origin
 const TIMEOUT = 1000, DELAY = 5;
 document.getElementById('page-site-info').innerHTML +=
-'<div id="site-subtitle">' +
-    '<span id="timeout">' + String(DELAY) + '</span>' + '秒后自动跳转回主页' +
-'</div>';
+`<div id="site-subtitle"><span id="timeout">${String(DELAY)}</span>秒后自动跳转回主页</div>`;
 
 (function () {
     let _delay = DELAY;
